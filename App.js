70bf731803032db4103672185ad44ab91460019d
Miika Nordblad <bhm247@myy.haaga-helia.fi>
2025-08-24 19:43:34 +0300

    Laskin tehty

--- App.js
+++ App.js
@@ -1,12 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View, SafeAreaView } from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
+
+  const [value1, setValue1] = useState("");
+  const [value2, setValue2] = useState("");
+  const [result, setResult] = useState(null);
+
+  const toNumber = (s) => {
+    const n = Number((s || "").trim().replace(",", "."));
+    return isNaN(n) ? 0 : n;
+  };
+  
+
+  const addition = () => {
+    setResult(toNumber(value1) + toNumber(value2));
+  }
+  const subtraction = () => {
+    setResult(toNumber(value1) - toNumber(value2));
+  }
+  const reset = () => {
+    setValue1("");
+    setValue2("");
+    setResult(null);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <View>
+      {result !== null && <Text style={{fontSize: 24}}>Result: {result.toFixed(2)}</Text>}
       <StatusBar style="auto" />
-    </View>
+      </View>
+      <View>
+        <TextInput
+        placeholder="Value 1"
+        value={value1}
+        onChangeText={setValue1}
+        keyboardType="numeric"
+        style={styles.input}
+        />
+        <TextInput
+        placeholder="Value 2"
+        value={value2}
+        onChangeText={setValue2}
+        keyboardType="numeric"
+        style={styles.input}
+        />
+      </View>
+      <View style={{flexDirection: "row"}}>
+        <Button title="+" onPress={addition}/>
+        <Button title="-" onPress={subtraction}/>
+        <Button title="Reset" onPress={reset}/>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -17,4 +64,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    borderWidth: 1,
+    borderColor: "#000",
+    borderRadius: 5,
+    padding: 10,
+    marginVertical: 5,
+    width: 200,
+    fontSize: 18
+  }
 });
